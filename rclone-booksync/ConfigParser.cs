69c90eaccaa7965a872a73ba996574b252e574b3
intelli21c <intelli21c@mulli.kr>
2023-01-29 02:04:04 +0900

    project rename, config parser and autoload, major redesign

---
+++ rclone-booksync/ConfigParser.cs
@@ -0,0 +1,56 @@
+﻿namespace rclone_booksync
+{
+	internal static class ConfigParser
+	{
+		public class RemoteEntry
+		{
+			public string name;
+			public string user;
+
+			public RemoteEntry()
+			{ }
+		}
+
+		static RemoteEntry parseentry(string[] text, int start, int end)
+		{
+			RemoteEntry remoteEntry = new();
+			remoteEntry.name = text[start].Substring(1, text[start].Length - 2);
+			for (int i = start + 1; i < end; i++)
+			{
+				int p = text[i].IndexOf("=");
+				if (p == -1) continue;
+				string word = text[i].Substring(0, (text[i][p - 1] == ' ') ? (p - 1) : p);
+				string rest = text[i].Substring((text[i][p + 1] == ' ') ? (p + 2) : (p + 1),
+					(text[i][p + 1] == ' ') ? (text[i].Length - p - 2) : (text[i].Length - p - 1));
+				if (word == "user")
+					remoteEntry.user = rest;
+			}
+			return remoteEntry;
+		}
+		static List<RemoteEntry> parseentries(string[] text)
+		{
+			List<RemoteEntry> remoteEntries = new List<RemoteEntry>();
+			List<int> entryindex = new List<int>();
+			for (int i = 0; i < text.Length; i++)
+			{
+				if (text[i].StartsWith('[') && text[i].EndsWith(']'))
+				{
+					entryindex.Add(i);
+				}
+			}
+			for (int i = 0; i < entryindex.Count - 1; i++)
+			{
+				remoteEntries.Add(parseentry(text, entryindex[i], entryindex[i + 1] - 1));
+			}
+			remoteEntries.Add(parseentry(text, entryindex[entryindex.Count - 1], text.Length));
+			return remoteEntries;
+		}
+
+		public static List<RemoteEntry> load(string path)
+		{
+			string[] text = System.IO.File.ReadAllLines(path);
+			List<RemoteEntry> remoteEntries = parseentries(text);
+			return remoteEntries;
+		}
+	}
+}
---
+++ rclone-booksync/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace rclone_booksync
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.button5 = new System.Windows.Forms.Button();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button6 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.BackColor = System.Drawing.Color.Black;
+			this.richTextBox1.ForeColor = System.Drawing.Color.White;
+			this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.ReadOnly = true;
+			this.richTextBox1.Size = new System.Drawing.Size(776, 322);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(105, 374);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(141, 27);
+			this.textBox3.TabIndex = 4;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(694, 373);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(94, 29);
+			this.button3.TabIndex = 6;
+			this.button3.Text = "push";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(594, 373);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(94, 29);
+			this.button4.TabIndex = 7;
+			this.button4.Text = "pull";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Checked = true;
+			this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox1.Location = new System.Drawing.Point(507, 373);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(81, 24);
+			this.checkBox1.TabIndex = 8;
+			this.checkBox1.Text = "dry-run";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(253, 339);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(73, 29);
+			this.button5.TabIndex = 9;
+			this.button5.Text = "browse";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(59, 341);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(188, 27);
+			this.textBox4.TabIndex = 10;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 374);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(87, 20);
+			this.label1.TabIndex = 11;
+			this.label1.Text = "profile:path";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 344);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(41, 20);
+			this.label3.TabIndex = 13;
+			this.label3.Text = "local";
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(203, 405);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(73, 29);
+			this.button6.TabIndex = 14;
+			this.button6.Text = "browse";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(69, 405);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(128, 27);
+			this.textBox2.TabIndex = 15;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 409);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(51, 20);
+			this.label4.TabIndex = 16;
+			this.label4.Text = "rclone";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(445, 374);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(56, 27);
+			this.textBox5.TabIndex = 17;
+			this.textBox5.Text = "-vP";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(403, 377);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(41, 20);
+			this.label5.TabIndex = 18;
+			this.label5.Text = "flags";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(680, 405);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(52, 29);
+			this.button1.TabIndex = 19;
+			this.button1.Text = "clear";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(738, 409);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(39, 20);
+			this.label2.TabIndex = 20;
+			this.label2.Text = "IDLE";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(632, 405);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(43, 29);
+			this.button2.TabIndex = 21;
+			this.button2.Text = "kill";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Enabled = false;
+			this.textBox1.Location = new System.Drawing.Point(340, 406);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(115, 27);
+			this.textBox1.TabIndex = 22;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(282, 409);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(52, 20);
+			this.label6.TabIndex = 23;
+			this.label6.Text = "config";
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Checked = true;
+			this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox2.Location = new System.Drawing.Point(533, 407);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(79, 24);
+			this.checkBox2.TabIndex = 24;
+			this.checkBox2.Text = "default";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+			// 
+			// domainUpDown1
+			// 
+			this.domainUpDown1.Location = new System.Drawing.Point(321, 372);
+			this.domainUpDown1.Name = "domainUpDown1";
+			this.domainUpDown1.Size = new System.Drawing.Size(76, 27);
+			this.domainUpDown1.TabIndex = 26;
+			this.domainUpDown1.Text = "0";
+			this.domainUpDown1.SelectedItemChanged += new System.EventHandler(this.domainUpDown1_SelectedItemChanged);
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(461, 404);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(66, 29);
+			this.button7.TabIndex = 27;
+			this.button7.Text = "profile";
+			this.button7.UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(694, 340);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(94, 29);
+			this.button8.TabIndex = 28;
+			this.button8.Text = "force-push";
+			this.button8.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.AutoSize = true;
+			this.checkBox3.Location = new System.Drawing.Point(253, 373);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(62, 24);
+			this.checkBox3.TabIndex = 29;
+			this.checkBox3.Text = "auto";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.checkBox3);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.domainUpDown1);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.richTextBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "RClone Booksync";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private OpenFileDialog openFileDialog1;
+		private RichTextBox richTextBox1;
+		private TextBox textBox3;
+		private Button button3;
+		private Button button4;
+		private CheckBox checkBox1;
+		private Button button5;
+		private TextBox textBox4;
+		private Label label1;
+		private Label label3;
+		private Button button6;
+		private TextBox textBox2;
+		private Label label4;
+		private FolderBrowserDialog folderBrowserDialog1;
+		private TextBox textBox5;
+		private Label label5;
+		private Button button1;
+		private Label label2;
+		private Button button2;
+		private TextBox textBox1;
+		private Label label6;
+		private CheckBox checkBox2;
+		private DomainUpDown domainUpDown1;
+		private Button button7;
+		private Button button8;
+		private CheckBox checkBox3;
+	}
+}
